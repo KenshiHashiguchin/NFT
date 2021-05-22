0a48ddeab31b5eab5bb29b088db17a7d8cf67e51
KenshiHashiguchin <kenshi.hashiguchi@gmail.com>
2021-05-22 20:12:23 +0900

    add .editconfig

---
+++ application/models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    status: DataTypes.INTEGER,
+    address: DataTypes.STRING,
+    tx_hash: DataTypes.STRING,
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
